6b7507dfcbbab58149aa3a6bca83d15629485f62
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-14 17:13:11 -0300

    Agrego modelo User.

---
+++ test/models/user_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+
+  test 'Un usuario con datos correctos es valido' do
+    user = User.new( email: 'user@test.cero', password_digest: 'secret_passwd')
+    assert user.valid?
+  end
+
+  test 'Un usuario con datos incorrectos es invalido.' do
+    user = User.new(email: 'usertestone', password_digest: 'secret_passwd')
+    assert_not user.valid?
+  end
+
+  test 'Un usuario con datos duplicados es invalido' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'secret_passwd')
+    assert_not user.valid?
+  end
+
+end
