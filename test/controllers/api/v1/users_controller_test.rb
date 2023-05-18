16b2e2317137d5be794d36a325f58077a9cb0d3e
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-18 15:39:33 -0300

    Agrego restricciones de acciones para usuarios no autorizados.

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -28,20 +28,32 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'Con los datos correctos se puede actualizar un usuario' do
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: 'secret_passwd' } }, as: :json
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: 'secret_passwd' } }, headers: {Authorization: JsonWebToken.encode(user_id: @user.id)}, as: :json
     assert_response :success
   end
 
   test 'Con los datos incorrectos no se puede actualizar un usuario' do
-    patch api_v1_user_url(@user), params: { user: { email: 'usertestcero', password: 'secret_passwd' } }, as: :json
+    patch api_v1_user_url(@user), params: { user: { email: 'usertestcero', password: 'secret_passwd' } }, headers: {Authorization: JsonWebToken.encode(user_id: @user.id)}, as: :json
     assert_response :unprocessable_entity
+  end
+
+  test 'Sin autorizacion no actulizar el usuario' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
   end
 
   test 'Eliminar un usuario' do
     assert_difference('User.count', -1) do
-      delete api_v1_user_url(@user), as: :json
+      delete api_v1_user_url(@user), headers: {Authorization: JsonWebToken.encode(user_id: @user.id)}, as: :json
     end
     assert_response :no_content
+  end
+
+  test 'No eliminar un usuario' do
+    assert_no_difference('User.count', -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 
 end
