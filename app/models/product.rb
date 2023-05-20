15ca911b9b12398800a97246be2639b0c9fe089d
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-20 10:42:56 -0300

    Agrego validaciones al modelo Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  validates :user_id, :title, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0}, presence: true
   belongs_to :user
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'los precios no pueden ser negativos' do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
 end
