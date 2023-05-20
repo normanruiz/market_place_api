d39495d94df67ca9cafe09c1f61ed6253015f13b
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-20 13:08:26 -0300

    Agrego controlador products, y su metodo show.

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ProductsController < ApplicationController
+
+    def show
+        render json: Product.find(params[:id])
+    end
+
+end
---
+++ test/controllers/api/v1/products_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    @product = products(:one)
+  end
+
+  test 'Devolver un producto' do
+    get api_v1_product_url(@product), as: :json
+    assert_response :success
+    json_response = JSON.parse(self.response.body)
+    assert_equal @product.title, json_response['title']
+  end
+
+end
