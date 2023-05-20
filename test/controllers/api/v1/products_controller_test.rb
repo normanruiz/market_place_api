31857c59543b2119787f0d3f257d9ad62d969fb3
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-20 14:34:23 -0300

    Agrego al controlador products el metodo index.

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -6,6 +6,11 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
+  test 'Devolver el listado de productos' do
+    get api_v1_products_url(), as: :json
+    assert_response :success
+  end
+
   test 'Devolver un producto' do
     get api_v1_product_url(@product), as: :json
     assert_response :success
