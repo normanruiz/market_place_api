c05defacca69c3d158b608e554872178c34ad9bf
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-05-23 17:00:17 -0300

    Agrego recuperacion de productos de usuario.

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -6,11 +6,13 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test 'Devolver un usuario con id 1' do
+  test 'Devolver un usuario' do
     get api_v1_user_url(@user), as: :json
     assert_response :success
-    json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['data']['attributes']['email']
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal @user.email, json_response.dig(:data, :attributes, :email)
+    assert_equal @user.products.first.id.to_s, json_response.dig(:data, :relationships, :products, :data, 0, :id)
+    assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
   end
 
   test 'Con los datos correctos crear un usuario' do
